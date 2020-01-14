f81d8975215a301662742a12c6fe79142b64d4f8
novanlexiku <novan.l@students.amikom.ac.id>
2020-01-14 17:59:44 +0700

    update profile form

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -63,7 +63,28 @@
         <v-divider></v-divider>
 
         <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6">
+          <v-text-field
+            :value="user.password"
+            label="Password"
+            type="password"
+            outlined
+            readonly
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            :value="user.no_ktp"
+            label="No KTP"
+            outlined
+            readonly
+          ></v-text-field>
+        </v-col>
+            </v-row>
+          </v-container>
+          
         </v-card-text>
       </div>
     </v-expand-transition>
@@ -100,8 +121,8 @@
 
         <v-col cols="12" sm="8">
           <v-text-field
-            :value="user.no_ktp"
-            label="Nomor KTP"
+            :value="user.no_hp"
+            label="Nomor Telepon"
             outlined
             readonly
           ></v-text-field>
